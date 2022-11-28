0dc12a1da21b510d730e6a443c2139c576b18cd3
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-11-28 15:31:05 +0900

    1 file modified, 1 added, 1 deleted

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom/client';
-import { StyledEngineProvider } from '@mui/material/styles';
-import App from './MultilineTextFields';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.querySelector('#root')).render(
-  <React.StrictMode>
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
