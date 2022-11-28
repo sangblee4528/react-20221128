6fd208f65e1e38ef79faa772b4df070231ecb597
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-11-28 11:55:32 +0900

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './style.css';
 import BasicTextFields from './BasicTextFields';
+import Button from '@mui/material/Button';
 
 export default function App() {
   const [textValue, setTextValue] = useState('');
@@ -8,15 +9,29 @@ export default function App() {
     setTextValue(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Submit');
+  };
+
+  const handleButtonClick = (e) => {
+    alert('Clicked');
+  };
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <textarea
-        placeholder="여기에 입력하세요."
-        value={textValue}
-        onChange={(e) => handleSetValue(e)}
-      ></textarea>
+      <from onSubmit={handleSubmit}>
+        <textarea
+          placeholder="여기에 입력하세요."
+          value={textValue}
+          onChange={(e) => handleSetValue(e)}
+        ></textarea>
+        <Button variant="contained" onClick={handleButtonClick}>
+          Click me
+        </Button>
+      </from>
       <p>{textValue}</p>
     </div>
   );
