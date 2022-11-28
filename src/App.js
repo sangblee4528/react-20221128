94bcef13608695a2eada8079b8dc760887dbd436
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-11-28 15:55:27 +0900

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './style.css';
 import MultilineTextFields from './MultilineTextFields';
 import Hello from './Hello';
+import Counter from './Counter';
 
 export default function App() {
   return (
     <div>
       <Hello name="react" color="blue" />
       <Hello />
+      <Counter />
       <p>Start editing to see some magic happen :)</p>
       <MultilineTextFields />
     </div>
