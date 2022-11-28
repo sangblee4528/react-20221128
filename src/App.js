4566aac1940a9097e5250f05cb43d7e434804865
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-11-28 10:51:23 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './style.css';
-import BasicTextFields from './BasicTextFiedls';
+import BasicTextFields from './BasicTextFields';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <BasicTextFields />
+      <BasicTextFiBasic />
     </div>
   );
 }
