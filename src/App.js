d40e88e0d7486fd8db30e434159892cdcc6fcff5
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-11-28 14:44:47 +0900

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import './style.css';
 
+import MultilineTextFields from './MultilineTextFields';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-
+      <MultilineTextFields />
     </div>
   );
 }
