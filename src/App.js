e643fdc2cc6344be018154147b4aeda48a366792
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-11-28 11:07:09 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import BasicTextFields from './BasicTextFields';
 
 export default function App() {
+  const [textValue, setTextValue] = useState('');
+  const handleSetValue = (e) => {
+    setTextValue(e.target.value);
+  };
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <BasicTextFiBasic />
+      <textarea
+        placeholder="여기에 입력하세요."
+        value={textValue}
+        onChange={(e) => handleSetValue(e)}
+      ></textarea>
+      <p>{textValue}</p>
     </div>
   );
 }
