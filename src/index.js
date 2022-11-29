20aba366b44496830f1d1b41cfdbe242b92d9bd5
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-11-29 16:45:51 +0900

    2 files modified

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+//import Counter from './Counter';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(<App />);
+//root.render(<Counter />);
