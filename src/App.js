30b4d11a7ba700548907062c8c5d06c1785cda82
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-11-29 16:21:56 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -19,16 +19,13 @@ function App() {
     email: '',
   });
   const { username, email } = inputs;
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setInputs({
-        ...inputs,
-        [name]: value,
-      });
-    },
-    [inputs]
-  );
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setInputs((inputs) => ({
+      ...inputs,
+      [name]: value,
+    }));
+  }, []);
 
   const [users, setUsers] = useState([
     {
@@ -59,30 +56,27 @@ function App() {
       email,
     };
     //setUsers([...users, user]);
-    setUsers(users.concat(user));
+    setUsers((users) => users.concat(user));
     setInputs({
       username: '',
       email: '',
     });
     nextId.current += 1;
-  }, [users, username, email]);
+  }, [username, email]);
 
-  const onRemove = useCallback(
-    (id) => {
-      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-      // = user.id 가 id 인 것을 제거함.
-      setUsers(users.filter((user) => user.id !== id));
-    },
-    [users]
-  );
+  const onRemove = useCallback((id) => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함.
+    setUsers((users) => users.filter((user) => user.id !== id));
+  }, []);
 
-  const onToggle = (id) => {
-    setUsers(
+  const onToggle = useCallback((id) => {
+    setUsers((users) =>
       users.map((user) =>
         user.id === id ? { ...user, active: !user.active } : user
       )
     );
-  };
+  }, []);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 
