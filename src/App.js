5562f229d3d24b22de6d04bb43910c6279934341
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-11-29 16:08:13 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo } from 'react';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 import './style.css';
 import MultilineTextFields from './MultilineTextFields';
 import Hello from './Hello';
@@ -19,13 +19,17 @@ function App() {
     email: '',
   });
   const { username, email } = inputs;
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+    },
+    [inputs]
+  );
+
   const [users, setUsers] = useState([
     {
       id: 1,
@@ -48,7 +52,7 @@ function App() {
   ]);
 
   const nextId = useRef(4);
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -61,13 +65,16 @@ function App() {
       email: '',
     });
     nextId.current += 1;
-  };
+  }, [users, username, email]);
 
-  const onRemove = (id) => {
-    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = user.id 가 id 인 것을 제거함.
-    setUsers(users.filter((user) => user.id !== id));
-  };
+  const onRemove = useCallback(
+    (id) => {
+      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+      // = user.id 가 id 인 것을 제거함.
+      setUsers(users.filter((user) => user.id !== id));
+    },
+    [users]
+  );
 
   const onToggle = (id) => {
     setUsers(
