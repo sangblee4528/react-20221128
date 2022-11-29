c0f5ca6c366f969dc62cdeb88c64e94f85c89fa0
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-11-29 18:02:31 +0900

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
-import React, { useRef, useState, useMemo, useCallback } from 'react';
-import './style.css';
-import MultilineTextFields from './MultilineTextFields';
-import Hello from './Hello';
-import Counter from './Counter';
+import React, { useRef, useReducer, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
-
-import InputSample from './InputSample';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
   return users.filter((user) => user.active).length;
 }
 
-function App() {
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs: {
     username: '',
     email: '',
-  });
-  const { username, email } = inputs;
-  const onChange = useCallback((e) => {
-    const { name, value } = e.target;
-    setInputs((inputs) => ({
-      ...inputs,
-      [name]: value,
-    }));
-  }, []);
-
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
       id: 1,
       username: 'velopert',
@@ -46,60 +31,24 @@ function App() {
       email: 'liz@example.com',
       active: false,
     },
-  ]);
+  ],
+};
 
-  const nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email,
-    };
-    //setUsers([...users, user]);
-    setUsers((users) => users.concat(user));
-    setInputs({
-      username: '',
-      email: '',
-    });
-    nextId.current += 1;
-  }, [username, email]);
+function reducer(state, action) {
+  return state;
+}
 
-  const onRemove = useCallback((id) => {
-    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = user.id 가 id 인 것을 제거함.
-    setUsers((users) => users.filter((user) => user.id !== id));
-  }, []);
-
-  const onToggle = useCallback((id) => {
-    setUsers((users) =>
-      users.map((user) =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
-  }, []);
-
-  const count = useMemo(() => countActiveUsers(users), [users]);
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { users } = state;
+  const { username, email } = state.inputs;
 
   return (
-    <div>
-      {/*}
-      <Hello name="react" color="blue" />
-      <Hello />
-      <Counter />
-      <InputSample />
-      */}
-
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
-      <div> 활성사용자 수 : {count} </div>
-      <p>Start editing to see some magic happen :)</p>
-      <MultilineTextFields />
-    </div>
+    <>
+      <<CreateUser username={username} email={email} />
+      <UserList users={users} />
+      <div>활성사용자 수 : 0</div>
+    </>
   );
 }
 
