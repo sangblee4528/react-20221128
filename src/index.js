268a058bfc8efc33816b9e97b3e3a97e0c4ee8ad
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-12-06 10:50:53 +0900

    2 files modified

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ const target = document.getElementById('target');
 // observe 설정 (MutationObserverInit)
 const config = { attributes: true, childList: true };
 
-
 // observer 인스턴스
 const observer = new MutationObserver((mutations) => {
   console.log('mutations', mutations);
+
+  const currentAction = mutations[0]; // 들어오는 변화의 첫 번째 것만 감지
+  // 해당 코드에서 Make Rea 만 attributes 이기 때문에 이렇게 작성했당.
+  if (currentAction.type === 'attributes') {
+    target.textContent = 'trigger';
+  }
 });
 
 // observe 대상 및 설정 전달
--- src/readMe.txt
+++ src/readMe.txt
@@ -1,3 +1,7 @@
 벨로퍼트와 함께하는 모던 리액트
 
 https://react.vlpt.us/basic/20-useReducer.html
+
+
+Observer 패턴 알아보기
+https://www.howdy-mj.me/javascript/observer-pattern
