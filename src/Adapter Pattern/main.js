3863156cf4b96bc7340aba890a23620450057c3f
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-12-06 12:15:35 +0900

    1 file modified, 1 added

--- src/Adapter Pattern/main.js
+++ src/Adapter Pattern/main.js
@@ -1,5 +1,5 @@
+/*
 import Printer from './Printer';
-import 
 
 let printer = new Printer();
 printer.pushText('Hello');
@@ -8,3 +8,13 @@ printer.pushText('Pattern');
 
 let result = printer.print();
 console.log(result);
+*/
+import HashTagPrinter from './HashTagPrinter';
+import HashTagAdapter from './HashTagAdapter';
+
+let printer = new HashTagAdapter(new HashTagPrinter());
+printer.pushText('Hello');
+printer.pushText('Design');
+printer.pushText('Pattern');
+
+console.log(printer.print());
