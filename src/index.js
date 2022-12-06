3d7f85754007a9a53d0e4d84f17237dda1ee3dea
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-12-06 10:07:14 +0900

    3 files modified

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import './style.css';
 
-import App from './App';
-//import Counter from './Counter';
-import AppContext from './AppContext';
+// observer 대상 node
+const target = document.getElementById('target');
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+// observe 설정 (MutationObserverInit)
+const config = { attributes: true, childList: true };
 
-//root.render(<App />);
-//root.render(<Counter />);
-root.render(<AppContext />);
+// observer 인스턴스
+const observer = new MutationObserver((mutations) => {
+  console.log('mutations', mutations);
+});
+
+// observe 대상 및 설정 전달
+document.querySelector('.start').addEventListener('click', () => {
+  console.log('Start Observe');
+  observer.observe(target, config);
+});
+
+// observe 중지
+document.getElementById('disconnect').addEventListener('click', () => {
+  console.log('Disconnect Observer');
+  observer.disconnect();
+});
+
+document.getElementById('attributes').addEventListener('click', () => {
+  target.setAttribute('class', 'red');
+});
+
+document.getElementById('childList').addEventListener('click', () => {
+  target.textContent = '변경'; // eotkddp
+});
+
+document.getElementById('reset').addEventListener('click', () => {
+  target.removeAttribute('class', 'red');
+  target.textContent = '';
+});
